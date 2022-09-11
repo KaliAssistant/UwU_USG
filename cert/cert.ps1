93f1385b13aa7608bdcb754b8c562c2f2ec04c01
KaliAssistant <96372398+KaliAssistant@users.noreply.github.com>
2022-09-11 17:25:44 +0800

    Add files via upload

---
+++ cert/cert.ps1
@@ -0,0 +1,3 @@
+﻿$cert = New-SelfSignedCertificate -DnsName Kali_Assistant -CertStoreLocation cert:\LocalMachine\My -type CodeSigning
+$pwd = ConvertTo-SecureString -String "helloworld" -Force -AsPlainText
+Export-PfxCertificate -cert $cert -FilePath mycert.pfx -Password $pwd
